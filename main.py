e26011ebbad0652ef0eb80048e5e404bdf42eb7a
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-03-18 19:21:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from pytgcalls.types import AudioPiped, HighQualityAudio
 from pyrogram.types import Message
 
 # --- CONFIG ---
-API_ID = 12345  # Apna API ID dalo
-API_HASH = "your_api_hash" # Apna API Hash dalo
-SESSION = "your_session" # Assistant ka String Session
+API_ID = 33603336  # Apna API ID dalo
+API_HASH = "c9683a8ec3b886c18219f650fc8ed429" # Apna API Hash dalo
+SESSION = "BQE-4i0ASxu8TXk4s870tFMn-D2Ijs-7DaTep8qcmRnZuowGYTiKDzzy9fKRT3pCc7aFI9oql0Rp5k1FkymDhRbewYPN11p5G7exMCs-z2bdMPuRoJCF60r7p_xq0TBjtLw5P1f-pXHHRxeXSAq0nKyNglv2pZ-GVCbYL4J-OwIkfck4wZyfiU0H58LZla5Il4VmVww-ewK3roa4mVjIxGKYoFva7LqYEf9Iti77jLz7HW7gCfuNessLDXqH1se4DuOSmoJzbacJxofENDQJChGjP4K7gbkMQQKwjCQfndvTmHLyDnc5jDqwfngZK1ogepmyiXZhhzHVebIieznK4DXTM1Q7pAAAAAHKarFXAA"
+# --- Random image links ---" # Assistant ka String Session
 
 app = Client("UkhiBot", api_id=API_ID, api_hash=API_HASH, session_string=SESSION)
 call = PyTgCalls(app)
